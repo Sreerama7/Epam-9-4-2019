14e0457a7d8d6f52d58fa843ae4797a7178251c7
srama <csr5526@gmail.com>
2019-05-07 13:55:36 +0530

    Commit

--- ShortestReach.java
+++ ShortestReach.java
@@ -1,6 +1,6 @@
 
 import java.util.*;
-public class ShortestSearch {
+public class ShortestReach {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int test = input.nextInt();
